22f5c76e1f5a02682c596c6c72925812e10b0c69
kanchichauhan <k.chauhan27199@gmail.com>
2024-07-04 00:12:49 +0530

    cart page ready

--- pizza-react/src/components/Cart.js
+++ pizza-react/src/components/Cart.js
@@ -2,8 +2,11 @@ import Header from "./Header"
 import { useContext, useEffect, useState } from "react";
 import { CartPizzaQty, PizzaData } from "../App";
 import { PizzaAddedToCart } from '../App'
+import './../App.css'
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
+    const navigate = useNavigate();
     const [cartData, setCartData] = useState([]);
     const cartQuantity = useContext(CartPizzaQty);
     const pizzaId = useContext(PizzaAddedToCart);
@@ -11,15 +14,12 @@ const Cart = () => {
 
     useEffect(() => {
         if (cartQuantity.cartQty > 0) {
-            console.log('pizzaAdded', pizzaId.pizzaAdded)
-            pizzaId.pizzaAdded.forEach((eachId) => {
-                const findData = pizzaData.find((eachData) => eachData.id === eachId);
-                console.log('findData', findData)
-                setCartData([...cartData, findData])
+            const updatedCartData = pizzaId.pizzaAdded.map((eachId) => {
+                return pizzaData.find((eachData) => eachData.id === eachId);
             });
-            console.log('cartData', cartData)
+            setCartData(updatedCartData);
         }
-    }, []);
+    }, [cartQuantity.cartQty, pizzaId.pizzaAdded, pizzaData]);
 
     const deleteCartQuantity = (pizzaId) => {
         console.log(pizzaId)
@@ -27,24 +27,26 @@ const Cart = () => {
 
     return (
         <>
+            <button onClick={() => navigate('/')}>Back to homepage</button>
             <Header cartQty={cartQuantity.cartQty} />
-            <div>Cart</div>
-            {/* {(cartQuantity.cartQty > 0) && cartData.map((eachPizza) => {
+            {console.log(cartQuantity.cartQty, cartData)}
+            <ul className='pizza-wrapper'>
+            {(cartQuantity.cartQty > 0) && cartData.map((eachPizza) => (
                 <li key={eachPizza.id}>
-                <img src={eachPizza.image} alt="pizza-image"/>
-                <div className='pizzaDetails'>
-                  <div className='pizza-name'>{eachPizza.name}</div>
-                  <div className='pizza-price'>{eachPizza.price}</div>
-                  <div className='pizza-rank'>{eachPizza.rank}</div>
-                  <div className='topping'>
-                  {eachPizza?.topping && eachPizza?.topping.map((eachTopping) => {
-                    (<span>{eachTopping}</span>)
-                  })}
-                  </div>
-                  <button className='addCart' onClick={() => deleteCartQuantity(eachPizza.id)}>Delete</button>
+                <img src={eachPizza.image} alt="pizza-image" /><div className='pizzaDetails'>
+                    <div className='pizza-name'>{eachPizza.name}</div>
+                    <div className='pizza-price'>{eachPizza.price}</div>
+                    <div className='pizza-rank'>{eachPizza.rank}</div>
+                    <div className='topping'>
+                        {eachPizza?.topping && eachPizza?.topping.map((eachTopping) => {
+                            (<span>{eachTopping}</span>);
+                        })}
+                    </div>
+                    <button className='addCart' onClick={() => deleteCartQuantity(eachPizza.id)}>Delete</button>
                 </div>
-              </li>
-            })} */}
+                </li>
+            ))}
+            </ul>
         </>
     )
 }
